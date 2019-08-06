c46cbd7b771d28c5c02821818d0cd0d83433dc97
xlifems <adrianmasapico@gmail.com>
2019-08-06 15:07:11 -0500

    Update utilities.js

--- JavaScript/utilities.js
+++ JavaScript/utilities.js
@@ -5,3 +5,20 @@ function isPar(numero) {
         return false;
     }
 }
+
+
+// Funcion para filtrar por valores unicos 
+
+const calculateUnicos = (value, index, datas) => {
+  return datas.indexOf(value) === index
+}
+
+const numbers = [25, 30, 30, 40, 25, 20, 25, 25, 10]
+const unicosNumbers = numbers.filter(calculateUnicos)
+console.log(uniqueAges)
+
+
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const result = words.filter(word => word.length > 6);
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
